4d72d0c6bc49c0e2545351c5ccd56c9ab25e7850
dan <yell@dracu.la>
2021-12-02 18:11:56 -0800

    Better pre-commit rules and bitta linting

--- whiteboard/scripts/run_tests.sh
+++ whiteboard/scripts/run_tests.sh
@@ -5,7 +5,7 @@ set -e
 # as it sets the working directory to an obscure directory instead of /deploy/code
 cd /deploy/code
 
-# There is a .coveragerc file which contains exclusions as well as output directory configuration 
+# There is a .coveragerc file which contains exclusions as well as output directory configuration
 echo "Running backend tests"
 pytest --durations=10 --cov=whiteboard --cov-report html:./test-coverage --cov-report xml:./test-coverage/coverage.xml --junitxml=./test-reports/test_report.xml whiteboard/tests
 
