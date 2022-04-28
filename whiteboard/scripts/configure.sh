6b54496a221018a633cc13c3489865debb50d486
dan <yell@dracu.la>
2022-04-27 20:11:35 -0700

    test

--- whiteboard/scripts/configure.sh
+++ whiteboard/scripts/configure.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+set -e
+
+echo PORT $PORT
+sed -i -e 's|PORT|'$PORT'|g' /etc/nginx/sites-available/app.conf
+
 supervisorctl -c /etc/supervisor/conf.d/supervisord.conf update
 supervisorctl -c /etc/supervisor/conf.d/supervisord.conf start uwsgi
