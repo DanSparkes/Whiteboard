88ce5fd8cb645f72358bd82e7a93de4ecb10f09b
dan <yell@dracu.la>
2022-04-27 20:20:02 -0700

    test

--- whiteboard/scripts/configure.sh
+++ whiteboard/scripts/configure.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-set -e
-
-echo PORT $PORT
-sed -i -e 's|PORT|'$PORT'|g' /etc/nginx/sites-available/app.conf
-
 supervisorctl -c /etc/supervisor/conf.d/supervisord.conf update
 supervisorctl -c /etc/supervisor/conf.d/supervisord.conf start uwsgi
--- whiteboard/scripts/set_port.sh
+++ whiteboard/scripts/set_port.sh
@@ -2,5 +2,4 @@
 set -e
 
 echo PORT $PORT
-sleep 5
 sed -i -e 's|PORT|'$PORT'|g' /etc/nginx/sites-available/app.conf
