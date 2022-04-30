da57e0656d717855b849b82f4481ffaf953d061c
dan <yell@dracu.la>
2022-04-30 15:16:08 -0700

    I hate heroku

--- whiteboard/settings.py
+++ whiteboard/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
