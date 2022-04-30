6c21dc6594793ce7dd126d463456fbd93cfecdb0
dan <yell@dracu.la>
2022-04-29 23:25:11 -0700

    roll with postgres

--- whiteboard/settings.py
+++ whiteboard/settings.py
@@ -101,9 +101,13 @@ WSGI_APPLICATION = "whiteboard.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    },
 }
 
 
