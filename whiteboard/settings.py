c1fba63f35f99c2a8a22ab3f637dc20d836dd14f
dan <yell@dracu.la>
2022-04-30 14:42:23 -0700

    static is hard

--- whiteboard/settings.py
+++ whiteboard/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
