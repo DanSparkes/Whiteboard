67eb6912afddcee553a06b61e824fc8da75dfade
dan <yell@dracu.la>
2022-04-30 14:32:52 -0700

    typo

--- whiteboard/settings.py
+++ whiteboard/settings.py
@@ -158,7 +158,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
 ]
 
 LOGIN_REDIRECT_URL = "/"
