f90ebccfb42188a66febbc1e32e446c1c27fd9c4
dan <yell@dracu.la>
2022-04-30 11:01:50 -0700

    serve some static files hopefully

--- whiteboard/settings.py
+++ whiteboard/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 LOGIN_REDIRECT_URL = "/"
 
